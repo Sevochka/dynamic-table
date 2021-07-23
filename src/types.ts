dd50f648262db504eae365ad878863343cad7842
Vsevolod <sevkaplay@gmail.com>
2021-07-23 22:09:10 +0300

    Add types.ts

---
+++ src/types.ts
@@ -0,0 +1,17 @@
+type Tariff = {
+  [key: string]: {
+    year: number
+  }
+}
+type Car = {
+  mark: string,
+  model: string,
+  tariffs: Tariff[];
+}
+type TariffsList = string[];
+
+type ResponseCars = {
+  cars: Car[],
+  tariffs_list: string[];
+}
+export type {Tariff, Car, TariffsList, ResponseCars};
