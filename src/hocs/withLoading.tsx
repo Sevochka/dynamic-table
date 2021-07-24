60ccfa30f7468c586e6ccd62b9f48b782b32ca23
Vsevolod <sevkaplay@gmail.com>
2021-07-24 12:22:47 +0300

    Optimize withLoading hoc

--- src/hocs/withLoading.tsx
+++ src/hocs/withLoading.tsx
@@ -6,26 +6,25 @@ type HocProps = {
   [propName: string]: any;
 };
 
-const WithLoading = (Component: React.FC<any>) =>
-  function WihLoadingComponent({ isLoading, ...props }: HocProps): JSX.Element {
+const WithLoading = (Component: React.FC<any>) => {
+  const getLoadingContent = () => {
+    const content = [];
+    for (let i = 1; i <= 12; i++) {
+      content.push(
+        <div key={i} className={`sk-circle${i} sk-child`}/>
+      );
+    }
+    return content;
+  };
+
+  return ({isLoading, ...props}: HocProps): JSX.Element => {
     return !isLoading ? (
       <Component {...props} />
     ) : (
       <div className="sk-circle">
-        <div className="sk-circle1 sk-child"/>
-        <div className="sk-circle2 sk-child"/>
-        <div className="sk-circle3 sk-child"/>
-        <div className="sk-circle4 sk-child"/>
-        <div className="sk-circle5 sk-child"/>
-        <div className="sk-circle6 sk-child"/>
-        <div className="sk-circle7 sk-child"/>
-        <div className="sk-circle8 sk-child"/>
-        <div className="sk-circle9 sk-child"/>
-        <div className="sk-circle10 sk-child"/>
-        <div className="sk-circle11 sk-child"/>
-        <div className="sk-circle12 sk-child"/>
+        {getLoadingContent()}
       </div>
     );
   };
-
+}
 export default WithLoading;
